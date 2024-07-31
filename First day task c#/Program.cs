fe49c8e4e9b822221e0791260a430a1d9e83c29f
darkk <darkk@HAMZAWY>
2024-07-31 07:42:18 +0300

    Add project files.

---
+++ First day task c#/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Enter A Number");
+int number = int.Parse (Console.ReadLine());
+if (number > 0)
+    Console.WriteLine("The Number Is positive ");
+else if (number < 0)
+    Console.WriteLine("The Number is Negative");
+else Console.WriteLine("The Number Is Zero");
+
